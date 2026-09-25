8864c16bf7d55818f1f11f27baa61c7baa262f82
Mishkat Naveed <mishkatnaveed14@gmail.com>
2026-09-25 17:46:57 +0500

    Food Menu Application working start

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+
+  
+  return (
+    <div>
+      <h1 id="heading">Our fod menu Application</h1>
+      <h3>Our Menu</h3>
+      <p className="para">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum laborum
+        vero deleniti ipsum sequi ex sed illum! Eius, consectetur vero?
+      </p>
+    </div>
+  );
+}
+
+export default App;
