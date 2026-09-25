d1ea010ee6c7e9d523748e71c4c50fe5dd928c8d
Mishkat Naveed <mishkatnaveed14@gmail.com>
2026-09-25 17:52:52 +0500

    working

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import "./App.css";
 
 function App() {
-
-  
   return (
     <div>
       <h1 id="heading">Our fod menu Application</h1>
@@ -11,6 +9,56 @@ function App() {
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum laborum
         vero deleniti ipsum sequi ex sed illum! Eius, consectetur vero?
       </p>
+      <ul id="foodlist">
+        <li>
+          <img src="https://static.vecteezy.com/system/resources/thumbnails/080/772/437/small_2x/premium-chicken-biryani-with-grilled-drumsticks-photo.jpg" alt="" />
+          <h4>Biryani</h4>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, blanditiis!</p>
+          <p>Quantity: </p>
+        </li>
+        <li>
+          <img src="" alt="" />
+          <h4></h4>
+          <p></p>
+          <p>Quantity: </p>
+        </li>
+        <li>
+          <img src="" alt="" />
+          <h4></h4>
+          <p></p>
+          <p>Quantity: </p>
+        </li>
+        <li>
+          <img src="" alt="" />
+          <h4></h4>
+          <p></p>
+          <p>Quantity: </p>
+        </li>
+        <li>
+          <img src="" alt="" />
+          <h4></h4>
+          <p></p>
+          <p>Quantity: </p>
+        </li>
+        <li>
+          <img src="" alt="" />
+          <h4></h4>
+          <p></p>
+          <p>Quantity: </p>
+        </li>
+        <li>
+          <img src="" alt="" />
+          <h4></h4>
+          <p></p>
+          <p>Quantity: </p>
+        </li>
+        <li>
+          <img src="" alt="" />
+          <h4></h4>
+          <p></p>
+          <p>Quantity: </p>
+        </li>
+      </ul>
     </div>
   );
 }
