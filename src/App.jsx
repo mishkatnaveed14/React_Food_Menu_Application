6c5bca669355aabe56a4c27702bb6e9262a5ace7
Mishkat Naveed <mishkatnaveed14@gmail.com>
2026-09-25 18:06:26 +0500

    working

--- src/App.jsx
+++ src/App.jsx
@@ -4,33 +4,35 @@ function App() {
   return (
     <div>
       <h1 id="heading">Our fod menu Application</h1>
-      <h3 style={
-        {
+      <h3
+        style={{
           textAlign: "center",
-          color:"AccentColor"
-        }
-      }>Our Menu</h3>
+          color: "AccentColor",
+        }}
+      >
+        Our Menu
+      </h3>
       <p className="para">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum laborum
         vero deleniti ipsum sequi ex sed illum! Eius, consectetur vero?
       </p>
       <ul id="foodlist">
-        <div>
-          <li>
-            <div id="imageArea">
-              <img
-                src="https://static.vecteezy.com/system/resources/thumbnails/080/772/437/small_2x/premium-chicken-biryani-with-grilled-drumsticks-photo.jpg"
-                alt=""
-              />
-            </div>
+        <li>
+          <div id="imageArea">
+            <img
+              src="https://static.vecteezy.com/system/resources/thumbnails/080/772/437/small_2x/premium-chicken-biryani-with-grilled-drumsticks-photo.jpg"
+              alt=""
+            />
+          </div>
+          <div>
             <h4>Biryani</h4>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus,
               blanditiis!
             </p>
             <p>Quantity: 0</p>
-          </li>
-        </div>
+          </div>
+        </li>
         {/* <li>
           <div>
             <img src="" alt="" />
