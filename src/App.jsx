e792d5ac4652c8d26d3996a0a06e5b38b78b65d6
Mishkat Naveed <mishkatnaveed14@gmail.com>
2026-09-25 18:00:49 +0500

    working

--- src/App.jsx
+++ src/App.jsx
@@ -10,20 +10,22 @@ function App() {
         vero deleniti ipsum sequi ex sed illum! Eius, consectetur vero?
       </p>
       <ul id="foodlist">
-        <li>
-         <div id="imageArea">
-           <img
-            src="https://static.vecteezy.com/system/resources/thumbnails/080/772/437/small_2x/premium-chicken-biryani-with-grilled-drumsticks-photo.jpg"
-            alt=""
-          />
-         </div>
-          <h4>Biryani</h4>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus,
-            blanditiis!
-          </p>
-          <p>Quantity: 0</p>
-        </li>
+        <div>
+          <li>
+            <div id="imageArea">
+              <img
+                src="https://static.vecteezy.com/system/resources/thumbnails/080/772/437/small_2x/premium-chicken-biryani-with-grilled-drumsticks-photo.jpg"
+                alt=""
+              />
+            </div>
+            <h4>Biryani</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus,
+              blanditiis!
+            </p>
+            <p>Quantity: 0</p>
+          </li>
+        </div>
         <li>
           <div>
             <img src="" alt="" />
