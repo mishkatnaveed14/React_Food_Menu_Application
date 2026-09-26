78f63f036feb06323cfdf5426925b891b6d2d627
Mishkat Naveed <mishkatnaveed14@gmail.com>
2026-09-26 14:41:56 +0500

    working

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,28 @@ function App() {
     <div>
       <HeadingComponent />
       <FoodList />
+      <Footer/>
     </div>
   );
 }
 
 export default App;
+function Footer() {
+  return(
+      <div style={
+      {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        
+      }
+    }>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, aut!</p>
+    <button style={{
+      backgroundColor: 'yellow',
+      border:'2px solid white'
+    }}>Order Now!</button>
+  </div>
+  )
+
+}
