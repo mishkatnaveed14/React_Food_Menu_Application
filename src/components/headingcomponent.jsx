987a2b4ad223d4f0b7a223a756276e6ada64d38c
Mishkat Naveed <mishkatnaveed14@gmail.com>
2026-09-26 14:35:07 +0500

    working

---
+++ src/components/headingcomponent.jsx
@@ -0,0 +1,19 @@
+export default function HeadingComponent() {
+  return (
+    <div>
+      <h1 id="heading">Our fod menu Application</h1>
+      <h3
+        style={{
+          textAlign: "center",
+          color: "AccentColor",
+        }}
+      >
+        Our Menu
+      </h3>
+      <p className="para">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum laborum
+        vero deleniti ipsum sequi ex sed illum! Eius, consectetur vero?
+      </p>
+    </div>
+  );
+}
