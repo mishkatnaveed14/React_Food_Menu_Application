ce7dfd97e670e1f0b590c806303bdf6e19c5bd5f
Mishkat Naveed <mishkatnaveed14@gmail.com>
2026-09-26 17:23:50 +0500

    working

---
+++ src/propspracyice.jsx
@@ -0,0 +1,26 @@
+export default function PropsPractice() {
+  return(
+   <div>
+     <Mybtn/>
+    <Mybtn/>
+    <Mybtn/>
+    <Mybtn/>
+   </div>
+  );
+}
+function Mybtn() {
+  return (
+    <div>
+      <button
+        style={{
+          width: "100px",
+          backgroundColor: "papayawhip",
+          border: "2px solid black",
+          borderRadius: "10px",
+        }}
+      >
+        A
+      </button>
+    </div>
+  );
+}
